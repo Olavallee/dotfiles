6637862960ef8fb3b863eba8140a621e05ea6079
Olivier L <lavallee.olivier@hotmail.com>
2024-09-11 18:39:18 -0400

    Modification de l'installation correction font et neovim

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-cd ~/
 mkdir cst
 cd cst
 mkdir bin cegep projets
 
-sudo pacman alacritty git vim nvim gcc gdb python3 python-pip python-virtualenv ttf-meslow-nerd polybar feh
+sudo pacman -Sy alacritty git vim neovim gcc gdb python3 python-pip python-virtualenv ttf-meslo-nerd polybar feh
 
-URL_REPO="https://github.com/Olavallee/dotfiles"
-URL_DEST_TEMP="/tmp/dotfile"
 
 #clone le repo git
-git clone $URL_REPO ~/
+if [ -d /tmp/dotfiles ]; then
+	sudo rm -rf /tmp/dotfiles
+fi
+
+# cloner dotfiles dans un fichier temporaire
+git clone https://github.com/Olavallee/dotfiles /tmp/dotfiles
 
 # copie config de dotfile pour remplacer ~/.config/i3/config
-cp URL_DEST_TEMP/config ~/.config/i3/config
-
-#Creer repertoire alacritty
-mkdir -p ~/.config/alacritty
+cp /tmp/dotfiles/config ~/.config/i3/config
 
 # copie le dossier alacritty du repo et le met dans ~/.config/
-cp -r $URL_DEST_TEMP/alacritty/* ~/.config/alacritty
+cp -r /tmp/dotfiles/alacritty ~/.config/alacritty
 
 # Remplace .bashrc par celui du repo git
-cp $URL_DEST_TEMP/.bashrc ~/.bashrc
+cp /tmp/dotfiles/.bashrc ~/.bashrc
 
 source ~/.bashrc
 
-rm -rf $URL_DEST_TEMP
-
 echo "Configuration terminee."
+
+sudo rm -rf ~/tmp/dotfilest
