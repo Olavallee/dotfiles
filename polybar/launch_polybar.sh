99b06854c5423732851628f7d4490287c9717a5c
Olivier Lavallée <112490941+Olavallee@users.noreply.github.com>
2024-09-13 19:20:53 -0400

    Update launch_polybar.sh missing =

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -2,7 +2,7 @@
 
 if type "xrandr" > /dev/null 2>&1; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config~/dotfiles/polybar/config.ini --reload toph &
+    MONITOR=$m polybar --config=~/dotfiles/polybar/config.ini --reload toph &
   done
 else
   polybar --config=~/dotfiles/polybar/config.ini --reload toph &
