e03cfeca2639c3f2f4c81045988cb41815ae2473
Olivier Lavallée <112490941+Olavallee@users.noreply.github.com>
2024-09-13 00:59:17 -0400

    Update et correction install.sh

--- install.sh
+++ install.sh
@@ -4,35 +4,25 @@ mkdir cst
 cd cst
 mkdir bin cegep projets
 
-sudo pacman -Sy alacritty git vim neovim gcc gdb python3 python-pip python-virtualenv ttf-meslo-nerd polybar feh
-
-
-#clone le repo git
-if [ -d /tmp/dotfiles ]; then
-	sudo rm -rf /tmp/dotfiles;
-fi
-
-if [ -f ~/.gitconfig ]; then
-	sudo rm -rf ~/.gitconfig;
-fi
+sudo pacman -Sy firefox alacritty git vim neovim gcc gdb python3 python-pip python-virtualenv ttf-meslo-nerd polybar feh xorg-xrandr
 
 # cloner dotfiles dans un fichier temporaire
-git clone https://github.com/Olavallee/dotfiles /tmp/dotfiles
+#git clone https://github.com/Olavallee/dotfiles /tmp/dotfiles
 
 # clone le .gitconfig a la place de ~/.gitconfig originel
-cp /tmp/dotfiles/.gitconfig ~/
+cp ~/dotfiles/.gitconfig ~/.gitconfig
 
 # copie config de dotfile pour remplacer ~/.config/i3/config
-cp /tmp/dotfiles/config ~/.config/i3/config
+cp ~/dotfiles/config ~/.config/i3/config
 
 # copie le dossier alacritty du repo et le met dans ~/.config/
-cp -r /tmp/dotfiles/alacritty ~/.config/alacritty
+cp -r ~/dotfiles/alacritty ~/.config/alacritty
 
 # Remplace .bashrc par celui du repo git
-cp /tmp/dotfiles/.bashrc ~/.bashrc
+cp ~/dotfiles/.bashrc ~/.bashrc
 
 # ajout du background dans image
-cp -r /tmp/dotfiles/background ~/Image/background
+cp -r ~/dotfiles/background ~/background
 
 source ~/.bashrc
 
